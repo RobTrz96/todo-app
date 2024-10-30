a4fb40e52949160b7ee34f07b6987d458f5a02ae
RobTrz96 <robert.trzeciakowski@gmail.com>
2024-10-30 01:09:34 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+  
+  todoList : TodoItem [] = [];
+  newTask:string=''
+
+  addTask():void {
+    if(this.newTask.trim() !== ''){
+      const newTodoItem : TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+    }
+  }
+
 }
